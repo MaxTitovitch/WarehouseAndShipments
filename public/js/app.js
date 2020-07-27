130c450ca2391c718e030b595d3bd17e835ef992
MaxTitovitch <maxtitovitch@mail.ru>
2020-07-27 20:59:28 +0300

    Product query append

--- public/js/app.js
+++ public/js/app.js
@@ -5,3 +5,22 @@ $('#import-open').click(function (event) {
 $('#import-input').change(function (event) {
   $('#import-submit').click();
 });
+
+$(document).ready(() => {
+  let products = [];
+  $.ajax({
+    type: "GET",
+    url: "/api/product",
+    success: (data) => {
+      data.forEach((element) => {
+        products.push({[element.id]: element.name});
+      })
+      let productSelect = $('.product-select')[0];
+      if(productSelect) {
+        products.forEach((element) => {
+          productSelect.appendChild(`<option value="${element.id}">${element.name}<option/>`)
+        })
+      }
+    }
+  });
+});
